91dedd2770b2981705962d3d77ff33e5905f7d9b
Meitar Moscovitz <meitarm@gmail.com>
2013-05-16 04:34:07 -0400

    Explicitly specify this software is public domain.

--- src/okcupid-pat/OfyService.java
+++ src/okcupid-pat/OfyService.java
@@ -1,3 +1,17 @@
+/**
+ * Predator Alert Tool for OkCupid
+ *
+ * Written in 2013 by Meitar Moscovitz <meitar@maymay.net>
+ *
+ * To the extent possible under law, the author(s) have dedicated all copyright
+ * and related and neighboring rights to this software to the public domain
+ * worldwide. This software is distributed without any warranty.
+ *
+ * You should have received a copy of the CC0 Public Domain Dedication along
+ * with this software. If not, see
+ *     http://creativecommons.org/publicdomain/zero/1.0/
+ */
+
 package okcupid_pat;
 
 import com.googlecode.objectify.Objectify;
